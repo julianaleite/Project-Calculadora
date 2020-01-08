f69a62e8ddc13d35cfebaec6a1735893ceb6673b
Juliana <juliana.silva@rsinet.com.br>
2020-01-08 12:15:08 -0300

    add projeto

---
+++ Tests/AppTests/AddingNumbersTests.java
@@ -0,0 +1,25 @@
+package AppTests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import source.Calculator;
+
+public class AddingNumbersTests {
+
+	private Calculator myCalculator = new Calculator();
+
+	@Test
+	public void addTwoPositiveNumbers() {
+		int expectedResult = 30;
+		int ActuaResult = myCalculator.Add(10, 20);
+		Assert.assertEquals("The the sum of two positive numbers is correct", expectedResult, ActuaResult);
+	}
+
+	@Test
+	public void addTwoNegativeNumbers() {
+		int expectedResult = -30;
+		int ActuaResult = myCalculator.Add(-10, -20);
+		Assert.assertEquals("The the sum of two negative numbers is correct", expectedResult, ActuaResult);
+	}
+}
---
+++ src/source/Calculator.java
@@ -0,0 +1,11 @@
+package source;
+
+public class Calculator {
+
+	public int Add(int Nu1, int Nu2) {
+		// TODO Auto-generated method stub
+		return Nu1 + Nu2;
+	}
+
+
+}
